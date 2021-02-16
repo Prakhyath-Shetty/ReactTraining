efdc46d7b82a6248f59169adde9945f8ae76a474
Prakhyath Shetty <prakhyath.ps@aykansoft.com>
2021-02-16 16:43:23 +0530

    Initial Code

---
+++ src/app/components/DashBoard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function DashBoard() {
+    return (
+        <div>
+            <Link to="Product">Product Link</Link>
+        </div>
+    )
+}
